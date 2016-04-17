2959c8bcb44255adb4d17958ed878dee934685b9
Berkel256 <alexandra.berkel@gmx.de>
2016-04-17 15:19:52 +0200

    Remove raw files, when jpg doesn't exist anymore.

---
+++ RawFilesRemover/FileRemover.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace RawFilesRemover
+{
+    class FileRemover
+    {
+        private string WorkingDirectory;
+        public FileRemover()
+        {
+            WorkingDirectory = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        }
+
+        public List<string> GetFilesToRemove(string checkExtension, string compareExtension)
+        {
+            List<string> ckeckFiles = GetFilesOfType(checkExtension);
+            List<string> compareFiles = GetFilesOfType(compareExtension);
+
+            List<string> deleteFiles = new List<string>();
+            for (int deleteIndex = 0; deleteIndex < ckeckFiles.Count; deleteIndex++)
+            {
+                bool found = false;
+                for (int compareIndex = 0; compareIndex < compareFiles.Count; compareIndex++)
+                {
+                    string deleteFile = DeleteExtension(ckeckFiles[deleteIndex], checkExtension);
+                    string compareFile = DeleteExtension(compareFiles[compareIndex], compareExtension);
+                    if (deleteFile.Equals(compareFile))
+                    {
+                        found = true;
+                        compareFiles.RemoveAt(compareIndex);
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    deleteFiles.Add(ckeckFiles[deleteIndex]);
+                }
+            }
+            return deleteFiles;
+
+        }
+
+        internal void DeleteFiles(List<string> deleteFiles)
+        {
+            for (int index = 0; index < deleteFiles.Count; index++)
+            {
+                try
+                {
+                    File.Delete(deleteFiles[index]);
+                }
+                catch(System.IO.IOException)
+                {
+
+                }
+            }
+        }
+
+        private List<string> GetFilesOfType(string extension)
+        {
+            return Directory.GetFiles(WorkingDirectory, "*." + extension).ToList();
+        }
+
+        private string DeleteExtension(string file, string extension)
+        {
+            if (file.EndsWith(extension))
+            {
+                return file.Remove(file.Length - extension.Length - 1);
+            }
+            else
+            {
+                return file;
+            }
+        }
+    }
+}
---
+++ RawFilesRemover/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.IO;
+
+namespace RawFilesRemover
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FileRemover Remover;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Remover = new FileRemover();
+        }
+
+        private void button_startDelete_Clicked(object sender, RoutedEventArgs e)
+        {
+            string deleteExtension = textbox_deleteName.Text.Trim();
+            string compareExtension = textbox_compareName.Text.Trim();
+            List<string> deleteFiles = Remover.GetFilesToRemove(deleteExtension, compareExtension);
+
+            string message = "Es wurden " + deleteFiles.Count + " zum Löschen gefunden. Wirklich löschen?";
+            string caption = "Löschen?";
+            MessageBoxResult result = MessageBox.Show(this, message, caption, MessageBoxButton.YesNo);
+            if (result.Equals(MessageBoxResult.Yes))
+            {
+                Remover.DeleteFiles(deleteFiles);
+            }
+        }
+    }
+}
